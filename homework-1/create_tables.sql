88a49afe75140efce93a78ddb5dd30cf0f8a6146
shlegel <sveta.shlegel@mail.ru>
2023-06-30 18:26:10 +0300

    homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	las_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+)
+
